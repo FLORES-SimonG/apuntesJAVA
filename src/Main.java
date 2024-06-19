5c242f4fd50054cfd2c0f88b9bfb697a81c4e308
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-06-19 22:25:31 +0200

    creacion de archivos

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 //        System.out.println("Hello world!");
 //        System.out.println("Hola amigoooooo");
 //        System.out.println("Como andas che");
@@ -37,19 +39,38 @@ public class Main {
 //        // NOS PERMITE CONVERTIR EL ARRAY EN UN OBJETO
 //
 
-        Scanner scanner = new Scanner(System.in); // con esto leo la consola
-        System.out.println("INGRESA TU NOMBRE:");
-        String name = scanner.nextLine(); //para leer nombres
-        System.out.println("hola amigo, vos te llamás---> " + name);
-        System.out.println("INGRESA TU EDAD:");
-        int age =scanner.nextInt();
+//        Scanner scanner = new Scanner(System.in); // con esto leo la consola
+//        System.out.println("INGRESA TU NOMBRE:");
+//        String name = scanner.nextLine(); //para leer nombres
+//        System.out.println("hola amigo, vos te llamás---> " + name);
+//        System.out.println("INGRESA TU EDAD:");
+//        int age =scanner.nextInt();
+//
+//        if (age<18){
+//            System.err.println("la edad no puede ser negativa");
+//        }else {
+//            System.out.println("Bienvenido");
+//        }
+//        System.out.println("addios mundito");
+//        scanner.close(); // para cerrar el scanner, es buena practica hacer esto.
 
-        if (age<18){
-            System.err.println("la edad no puede ser negativa");
-        }else {
-            System.out.println("Bienvenido");
-        }
-        System.out.println("addios mundito");
-        scanner.close(); // para cerrar el scanner, es buena practica hacer esto.
+
+//        String[] arrayGrande = {"pedro", "arbol", "corazón", "amigo", "indigo", "calzón", "uranio"};
+//
+//        for (int i = 0; i < arrayGrande.length; i++) {
+//            char primeraLetra = arrayGrande[i].charAt(0);
+//            String vocales = "aeiou";
+//            if (vocales.indexOf(primeraLetra) != -1) {
+//                System.out.println(arrayGrande[i]);
+//            } else {
+//                System.out.println("letra no válida --> " + arrayGrande[i]);
+//            }
+//        }
+//       CREACIÓN DE ARCHIVOS
+        FileWriter writer = new FileWriter("demo.txt", true);
+        writer.write("Esto es un mensaje importante\n");
+        writer.write("Este es otro mensajito\n");
+        writer.close();
+
     }
 }
